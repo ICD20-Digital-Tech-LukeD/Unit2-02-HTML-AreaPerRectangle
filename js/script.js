ba1ab83a92d47ae3becda677f96a31bd439d8c4d
LukeDiBert <159164688+LukeDiBert@users.noreply.github.com>
2024-03-19 03:40:30 +0000

    stuff

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function doAreaClicked () {
+  // this function does all the math
+  document.getElementById('area-math').innerHTML = 'The area is ' + (5 * 3)
+}
+
+function doPerimeterClicked () {
+  document.getElementById('perimeter-math').innerHTML = 'The perimeter is  ' + (10 + 6)
+}
